915f710a00ef6c5b0c0f76226d748f1bd5780b72
JanxMx <38433932+JanxMx@users.noreply.github.com>
2018-04-27 17:25:16 -0500

    Update data - 03.js

--- data - 03.js
+++ data - 03.js
@@ -51,43 +51,10 @@ var data_en = {
 	"profileEnglish": "English (advanced), Italian (pre-intermediate) & French (elementary)",
 	"profileVisa": "American Visa for tourism (B1/B2)",
 	"profileOther": "Good team player, responsible, creative and proactive among other capabilities such as dedicated, patient and innovator.",
-
+				
 	"teCollection": [	
-				{"name": "Java"}, 
-				{"name": "Javascript"},
-				{"name": "JSON"},
-				{"name": "CSS"},
-				{"name": "HTML"},
-				{"name": "XML"},
-				{"name": "MVC"},
-				{"name": "Struts"},
-				{"name": "Spring"},
-				{"name": "Hibernate"},
-				{"name": "Swagger"},
-				{"name": "Eclipse"},
-				{"name": "STS"},
-				{"name": "Netbeans"},
-				{"name": "JDeveloper"},
-				{"name": "Oracle"},
-				{"name": "MsSQL Server"},
-				{"name": "MySQL"},
-				{"name": "Mongo"},
-				{"name": "Toad"},
-				{"name": "PL/SQL"},
-				{"name": "Maven"},
-				{"name": "Gradle"},
-				{"name": "JBoos"},
-				{"name": "Apache Tomcat"},
-				{"name": "Glashfish"},
-				{"name": "SVN"},
-				{"name": "Linux"},
-				{"name": "Patterns"},
-				{"name": "SpringBoot"},	
-				{"name": "Swagger"},	
-				{"name": "Docker"},	
-				{"name": "GitHub"},					
-				{"name": "UML"}],
-
+				"Java", "Javascript", "JSON", "CSS", "HTML", "XML", "MVC", "Struts", "Spring", "Hibernate", "Swagger", "Eclipse", "STS", "Netbeans", "JDeveloper", "Oracle", "MsSQL Server", "MySQL", "Mongo", "Toad", "PL/SQL", "Maven", "Gradle", "JBoos", "Apache Tomcat", "Glashfish", "SVN", "Linux", "Patterns", "SpringBoot", "Swagger", "Docker", "GitHub", "UML"],
+				
 	"tsCollection": [	
 				"System analyst",
 				"Web Programmer (Java & Oracle)",
@@ -181,61 +148,25 @@ var data_en = {
 				 "description": "It is a system of administration and configuration of parameters.",
 				 "objective": "The objective of the MC was to manage the parameters used by the NextGen system. The parameters have dependencies between them based on legal rules and commercial rules, which involved the creation of a complex algorithm to validate the rules.",
 				 "responsibilities": [
-					{"desc": "administration and planning of project activities."},
-					{"desc": "interaction with American users to follow up on requirements and defects."},
-					{"desc": "analysis, development, maintenance and testing of the different modules including the database part (back-end)."},
-					{"desc": "analysis, development, maintenance and testing of Restful services."},
-					{"desc": "configuration of local servers for testing."}],
+					"administration and planning of project activities.",
+					"interaction with American users to follow up on requirements and defects.",
+					"analysis, development, maintenance and testing of the different modules including the database part (back-end).",
+					"analysis, development, maintenance and testing of Restful services.",
+					"configuration of local servers for testing."],
 				 "usedTools":[
-					{"tool": "Eclipse"},
-					{"tool": "Java"},
-					{"tool": "Strut"},
-					{"tool": "Spring"},
-					{"tool": "Maven"},
-					{"tool": "HTML"},
-					{"tool": "JSP"},
-					{"tool": "Javascript"},
-					{"tool": "JQuery"},
-					{"tool": "CSS"},
-					{"tool": "SQL"},
-					{"tool": "Oracle"},
-					{"tool": "Toad"},
-					{"tool": "JBoss"},					
-					{"tool": "Jetty"},
-					{"tool": "JUnit"},
-					{"tool": "TDD"},
-					{"tool": "Scrum"}]
+					"Eclipse", "Java", "Strut", "Spring", "Maven", "HTML", "JSP", "Javascript", "JQuery", "CSS", "SQL", "Oracle", "Toad", "JBoss", "Jetty", "JUnit", "TDD", "Scrum"]
 				}, 
+				
 				{"company": "Banco Azteca",
 				 "project": "Various",
 				 "user": "LAM Systems (Latin American)",
 				 "description": "Support to different systems.",
 				 "objective": "This area was a type of internal systems consultancy for Banco Azteca - LAM, it consisted of providing support to Latin American systems.",
 				 "responsibilities": [
-					{"desc": "analysis, development, maintenance and testing of the different modules (back-end)."},
-					{"desc": "communication with users."} ],
+					"analysis, development, maintenance and testing of the different modules (back-end).",
+					"communication with users." ],
 				 "usedTools":[
-					{"tool": "Eclipse"},
-					{"tool": "Netbeans"},
-					{"tool": "JDeveloper"},
-					{"tool": "Java"},
-					{"tool": "Strut"},
-					{"tool": "Spring"},
-					{"tool": "Hibernate"},
-					{"tool": "Maven"},
-					{"tool": "HTML"},
-					{"tool": "JSP"},
-					{"tool": "Javascript"},
-					{"tool": "JQuery"},
-					{"tool": "ExtJs"},
-					{"tool": "DWR"},
-					{"tool": "CSS"},
-					{"tool": "SQL"},
-					{"tool": "Oracle"},
-					{"tool": "Toad"},
-					{"tool": "JBoss"},					
-					{"tool": "Glassfish"},
-					{"tool": "JUnit"}]
+					 "Eclipse", "Netbeans", "JDeveloper", "Java", "Strut", "Spring", "Hibernate", "Maven", "HTML", "JSP", "Javascript", "JQuery", "ExtJs", "DWR", "CSS", "SQL", "Oracle", "Toad", "JBoss",	 "Glassfish", "JUnit"]
 				}, 
 				 
 				{"company": "Softtek",
@@ -244,20 +175,10 @@ var data_en = {
 				 "description": "It was a validating system of accounting document formats.",
 				 "objective": "The objective of the system was to validate that the format of the accounting documents was valid, once validated the document was sent to the system in charge of processing it.",
 				 "responsibilities": [
-					{"desc": "analysis, development, maintenance and testing of the different modules (back-end)."},
-					{"desc": "the response performance of some database queries was improved."} ],
+					"analysis, development, maintenance and testing of the different modules (back-end).",
+					"the response performance of some database queries was improved." ],
 				 "usedTools":[
-					{"tool": "Eclipse"},
-					{"tool": "Java"},
-					{"tool": "HTML"},
-					{"tool": "JSP"},
-					{"tool": "Javascript"},
-					{"tool": "JQuery"},
-					{"tool": "CSS"},
-					{"tool": "SQL"},
-					{"tool": "Oracle"},
-					{"tool": "Toad"},
-					{"tool": "JBoss"}]
+					"Eclipse", "Java", "HTML", "JSP", "Javascript", "JQuery", "CSS", "SQL", "Oracle", "Toad", "JBoss"]
 				},
 					
 				{"company": "Softtek",
@@ -266,20 +187,19 @@ var data_en = {
 				 "description": "Migration of BD with information from GE-Realstate.",
 				 "objective": "The objective of the project was to validate and migrate information from an obsolete DB to an Oracle database. One of the main challenges was maintaining integrity in the catalogs which were updated two or three times a week. The other great challenge was to deliver the project within a period of 4 months because it had a delay of 2 years.",
 				 "responsibilities": [
-					{"desc": "responsible for the project."},
-					{"desc": "daily communication with the project user (in English)."},
-					{"desc": "reverse engineering."},
-					{"desc": "generation of scripts for validation and migration (SQL Loader)."},
-					{"desc": "flow analysis and redesign for time reduction."},
-					{"desc": "tests."}],
+					"responsible for the project.",
+					"daily communication with the project user (in English).",
+					"reverse engineering.",
+					"generation of scripts for validation and migration (SQL Loader).",
+					"flow analysis and redesign for time reduction.",
+					"tests."],
 				 "usedTools":[
-					{"tool": "SQL"},
-					{"tool": "Oracle"},
-					{"tool": "Toad"}]
+					"SQL", "Oracle", "Toad"]
 				}		
 	],
 	
-	"peTempCollection2": [	
+	"peCollection2": [	
+	
 				{"company": "Elektra (Banco Azteca and Advance America) ",
 				 "position": "Java Web Developer",
 				 "years": "8 years",
@@ -294,7 +214,7 @@ var data_en = {
 						"responsibilities": [
 							"administration and planning of project activities.",
 							"interaction with American users to follow up on requirements and defects.",
-							"analysis, development, maintenance and testing of the different modules including the database part (back-end).",
+							"analysis, development, maintenance and testing of the different modules including the database (back-end).",
 							"analysis, development, maintenance and testing of Restful services.","configuration of local servers for testing."],
 						"usedTools":[
 							"Eclipse", "Java", "Strut", "Spring", "Maven", "HTML", "JSP", "Javascript", "JQuery", "CSS", "SQL", "Oracle", "Toad", "JBoss", "Jetty", "JUnit", "TDD", "Scrum"]
@@ -319,24 +239,96 @@ var data_en = {
 				 "position": "Java Web Developer",
 				 "years": "7 months",
 				 "date": "2009-2010",
-				 "positions": []},
-				{"company": "Softtek (General Electric-Corporate)",
+				 "positions": [
+				 		{"businessUnit": "Tridium",
+						"position": "Java Web Developer",
+						"project": "Tridium",
+						"description": "Software for smart buildings",
+						"objective": 
+							"The objective was to work software for intelligent buildings on the Niagara AX framework as well as the creation and maintenance of the company's web portal.",
+						"responsibilities": [
+							"analysis, development, maintenance and testing of the web portal.",
+							"be certified in the Niagara AX framework by Tridium",
+							"Training of a group of people on the tridium framework"],
+						"usedTools":[
+							"Eclipse", "Java", "HTML", "JSP", "Javascript", "JQuery", "CSS"]
+						}
+					]},
+				 
+				{"company": "Softtek",
 				 "position": "Java Web Developer & DBA",
 				 "years": "2 years",
 				 "date": "2007-2009",
-				 "positions": []},
+				 "positions": [
+						{"businessUnit": "GE - Corporate",
+						"position": "Java Web Developer",
+						"project": "Mars - DXL",
+						"description": "It was a validating system of accounting document formats.",
+						"objective": 
+							"The objective of the system was to validate the format of the accounting documents, once validated the document was sent to the system in charge of processing it.",
+						"responsibilities": [
+							"analysis, development, maintenance and testing of the different modules (back-end).",
+							"improve the performance of some database queries."],
+						"usedTools":[
+							"Eclipse", "Java", "HTML", "JSP", "Javascript", "JQuery", "CSS", "SQL", "Oracle", "Toad", "JBoss"]
+						}, 
+						{"businessUnit": "GE - Corporate",
+						"position": "DBA",
+						"project": "RealEstate",
+						"description": "Migration of BD with information from GE-Realstate.",
+						"objective": 
+							"The objective of the project was to validate and migrate information from an obsolete DB to an Oracle database. One of the main challenges was maintaining integrity in the catalogs which were updated two or three times a week. The other great challenge was to deliver the project within a period of 4 months because it had a delay of 2 years.",
+						"responsibilities": [
+							"responsible for the project.",
+							"daily communication with the project user (in English).",
+							"reverse engineering.",
+							"generation of scripts for validation and migration (SQL Loader).",
+							"flow analysis and redesign for time reduction.",
+							"tests."],
+						"usedTools":[
+							"SQL", "Oracle", "Toad"]
+						}
+					]},
 				{"company": "Electoral Federal Institute",
 				 "position": "Delphi Developer & DBA",
 				 "years": "6 months",
 				 "date": "2006",
-				 "positions": []},
-				{"company": "ITESM Campus Aguascalientes",
+				 "positions": [
+						{"businessUnit": "IFE",
+						"position": "Delphi Developer & DBA",
+						"project": "Appointment management",
+						"description": "It was a system to manage appointments of the IFE.",
+						"objective": 
+							"It was a system to manage the appointments of people who needed to perform paperwork in the offices of the IFE.",
+						"responsibilities": [
+							"analysis, development, maintenance and testing of the system."],
+						"usedTools":[
+							"Delphi", "MsSQL Server"]
+						}
+					]},
+				{"company": "Cecytea",
 				 "position": "IT Support & Developer",
 				 "years": "5 years",
 				 "date": "2001-2006",
-				 "positions": []}]
-	
+				 "positions": [
+						{"businessUnit": "IT",
+						"position": "IT support assistant",
+						"project": "Various",
+						"description": "It was a managing employees system.",
+						"objective": 
+							"The objective of the system was to manage the information of the employees as well as assistance and control of classes.",
+						"responsibilities": [
+							"analysis, development, maintenance and testing of the system.",
+							"preventive and corrective maintenance of computer equipment",
+							"installation and configuration of networks."],
+						"usedTools":[
+							"Delphi", "MsSQL Server"]
+						}
+					]}
+					
+			]
 }
+/* "Science and Technology Studies Center of the State of Aguascalientes" */
 
 var data_es = {	
 	"langCollection": [	
@@ -373,48 +365,15 @@ var data_es = {
 	"profileOther": "Trabajo en equipo, responsable, creativo y proactivo entre otras habilidades como dedicado, paciente and innovador.",
 	
 	"teCollection": [	
-				{"name": "Java"}, 
-				{"name": "Javascript"},
-				{"name": "JSON"},
-				{"name": "CSS"},
-				{"name": "HTML"},
-				{"name": "XML"},
-				{"name": "MVC"},
-				{"name": "Struts"},
-				{"name": "Spring"},
-				{"name": "Hibernate"},
-				{"name": "Swagger"},
-				{"name": "Eclipse"},
-				{"name": "STS"},
-				{"name": "Netbeans"},
-				{"name": "JDeveloper"},
-				{"name": "Oracle"},
-				{"name": "MsSQL Server"},
-				{"name": "MySQL"},
-				{"name": "Mongo"},
-				{"name": "Toad"},
-				{"name": "PL/SQL"},
-				{"name": "Maven"},
-				{"name": "Gradle"},
-				{"name": "JBoos"},
-				{"name": "Apache Tomcat"},
-				{"name": "Glashfish"},
-				{"name": "SVN"},
-				{"name": "Linux"},
-				{"name": "Patterns"},
-				{"name": "SpringBoot"},	
-				{"name": "Swagger"},	
-				{"name": "Docker"},	
-				{"name": "GitHub"},					
-				{"name": "UML"}],	
+				"Java", "Javascript", "JSON", "CSS", "HTML", "XML", "MVC", "Struts", "Spring", "Hibernate", "Swagger", "Eclipse", "STS", "Netbeans", "JDeveloper", "Oracle", "MsSQL Server", "MySQL", "Mongo", "Toad", "PL/SQL", "Maven", "Gradle", "JBoos", "Apache Tomcat", "Glashfish", "SVN", "Linux", "Patterns", "SpringBoot", "Swagger", "Docker", "GitHub", "UML"],
 	
 	"tsCollection": [	
-				{"name": "Análista de Sistemas"}, 
-				{"name": "Programador Web (Java & Oracle)"},
-				{"name": "Análisis y creación de bases de datos (schemas, sp, triggers, otros)"},
-				{"name": "Validación de datos, migración y respaldos (scripts)"},
-				{"name": "Instalación y configuración de servidores (web, mail, ftp, otros)"},
-				{"name": "Pruebas de QA"}],
+				"Análista de Sistemas", 
+				"Programador Web (Java & Oracle)",
+				"Análisis y creación de bases de datos (schemas, sp, triggers, otros)",
+				"Validación de datos, migración y respaldos (scripts)",
+				"Instalación y configuración de servidores (web, mail, ftp, otros)",
+				"Pruebas de QA"],
 
 	"peTitleCompany": "Compañia",
 	"peTitlePosition": "Puesto",
@@ -527,48 +486,15 @@ var data_it = {
 	"profileOther": "Buon giocatore di squadra, responsabile, creativo e propositivo tra le altre capacità come dedicato, paziente e innovatore.",
 	
 	"teCollection": [	
-				{"name": "Java"}, 
-				{"name": "Javascript"},
-				{"name": "JSON"},
-				{"name": "CSS"},
-				{"name": "HTML"},
-				{"name": "XML"},
-				{"name": "MVC"},
-				{"name": "Struts"},
-				{"name": "Spring"},
-				{"name": "Hibernate"},
-				{"name": "Swagger"},
-				{"name": "Eclipse"},
-				{"name": "STS"},
-				{"name": "Netbeans"},
-				{"name": "JDeveloper"},
-				{"name": "Oracle"},
-				{"name": "MsSQL Server"},
-				{"name": "MySQL"},
-				{"name": "Mongo"},
-				{"name": "Toad"},
-				{"name": "PL/SQL"},
-				{"name": "Maven"},
-				{"name": "Gradle"},
-				{"name": "JBoos"},
-				{"name": "Apache Tomcat"},
-				{"name": "Glashfish"},
-				{"name": "SVN"},
-				{"name": "Linux"},
-				{"name": "Patterns"},
-				{"name": "SpringBoot"},	
-				{"name": "Swagger"},	
-				{"name": "Docker"},	
-				{"name": "GitHub"},					
-				{"name": "UML"}],	
-	
+				"Java", "Javascript", "JSON", "CSS", "HTML", "XML", "MVC", "Struts", "Spring", "Hibernate", "Swagger", "Eclipse", "STS", "Netbeans", "JDeveloper", "Oracle", "MsSQL Server", "MySQL", "Mongo", "Toad", "PL/SQL", "Maven", "Gradle", "JBoos", "Apache Tomcat", "Glashfish", "SVN", "Linux", "Patterns", "SpringBoot", "Swagger", "Docker", "GitHub", "UML"],	
+
 	"tsCollection": [	
-				{"name": "Analista di sistema"}, 
-				{"name": "Programmatore Web (Java e Oracle)"},
-				{"name": "Analisi e creazione di database (schemi, sp, trigger, altri)"},
-				{"name": "Convalida dei dati, migrazione e backup (script)"},
-				{"name": "Installazione e configurazione di server (web, mail, ftp, altri)"},
-				{"name": "Test del QA"}],
+				"Analista di sistema", 
+				"Programmatore Web (Java e Oracle)",
+				"Analisi e creazione di database (schemi, sp, trigger, altri)",
+				"Convalida dei dati, migrazione e backup (script)",
+				"Installazione e configurazione di server (web, mail, ftp, altri)",
+				"Test del QA"],
 
 	"peTitleCompany": "Azienda",
 	"peTitlePosition": "Posizione",
@@ -682,48 +608,15 @@ var data_fr = {
 	"profileOther": "Bon joueur d'équipe, responsable, créatif et proactif parmi d'autres capacités telles que dédié, patient et innovateur.",
 	
 	"teCollection": [	
-				{"name": "Java"}, 
-				{"name": "Javascript"},
-				{"name": "JSON"},
-				{"name": "CSS"},
-				{"name": "HTML"},
-				{"name": "XML"},
-				{"name": "MVC"},
-				{"name": "Struts"},
-				{"name": "Spring"},
-				{"name": "Hibernate"},
-				{"name": "Swagger"},
-				{"name": "Eclipse"},
-				{"name": "STS"},
-				{"name": "Netbeans"},
-				{"name": "JDeveloper"},
-				{"name": "Oracle"},
-				{"name": "MsSQL Server"},
-				{"name": "MySQL"},
-				{"name": "Mongo"},
-				{"name": "Toad"},
-				{"name": "PL/SQL"},
-				{"name": "Maven"},
-				{"name": "Gradle"},
-				{"name": "JBoos"},
-				{"name": "Apache Tomcat"},
-				{"name": "Glashfish"},
-				{"name": "SVN"},
-				{"name": "Linux"},
-				{"name": "Patterns"},
-				{"name": "SpringBoot"},	
-				{"name": "Swagger"},	
-				{"name": "Docker"},	
-				{"name": "GitHub"},					
-				{"name": "UML"}],
+				"Java", "Javascript", "JSON", "CSS", "HTML", "XML", "MVC", "Struts", "Spring", "Hibernate", "Swagger", "Eclipse", "STS", "Netbeans", "JDeveloper", "Oracle", "MsSQL Server", "MySQL", "Mongo", "Toad", "PL/SQL", "Maven", "Gradle", "JBoos", "Apache Tomcat", "Glashfish", "SVN", "Linux", "Patterns", "SpringBoot", "Swagger", "Docker", "GitHub", "UML"],
 	
 	"tsCollection": [	
-				{"name": "Analyste de système"}, 
-				{"name": "Programmeur Web (Java et Oracle)"},
-				{"name": "Analyse et création de bases de données (schémas, sp, triggers, autres)"},
-				{"name": "Validation des données, migration et sauvegarde (scripts)"},
-				{"name": "Installation et configuration des serveurs (web, mail, ftp, autres)"},
-				{"name": "Test d'assurance qualité"}],
+				"Analyste de système", 
+				"Programmeur Web (Java et Oracle)",
+				"Analyse et création de bases de données (schémas, sp, triggers, autres)",
+				"Validation des données, migration et sauvegarde (scripts)",
+				"Installation et configuration des serveurs (web, mail, ftp, autres)",
+				"Test d'assurance qualité"],
 	
 	"peTitleCompany": "Nom de l'entreprise",
 	"peTitlePosition": "Position",
